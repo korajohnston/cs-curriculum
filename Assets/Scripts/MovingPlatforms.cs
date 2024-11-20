ef88837020a55aee1cc161355a25b20aad2d2860
korajohnston <152660234+korajohnston@users.noreply.github.com>
2024-11-20 13:57:04 -0800

    platforms move!
    yayyyyyyyyyyyy

---
+++ Assets/Scripts/MovingPlatforms.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatforms : MonoBehaviour
+{
+    public List<GameObject> waypoints;
+    public int currentWaypoint = 0;
+    private float speed = 4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, waypoints[currentWaypoint].transform.position) < 1f)
+        {
+            currentWaypoint++;
+            currentWaypoint %= waypoints.Count;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[currentWaypoint].transform.position,
+            1.5f * Time.deltaTime);
+    }
+}
